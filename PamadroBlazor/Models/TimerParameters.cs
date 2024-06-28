4423729bb9a6c3ff701d46633bff3349a5915ecb
max <maxim.mihaluk@gmail.com>
2024-06-29 00:00:50 +0300

    1. Blazor web site created. 2. Online timer implementation started.

---
+++ PamadroBlazor/Models/TimerParameters.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PamadroBlazor.Models
+{
+    public class TimerParameters
+    {
+        [Required(ErrorMessage = "Please input Work Time")]
+        public int WorkTimeMin { get; set; } = 50;
+        public int ShortBreakTimeMin { get; set; } = 10;
+        public int LongBreakTimeMin { get; set; } = 20; // reserved for future use
+        public string TaskName { get; set; } = "Hellow World";
+        public bool PlaySound { get; set; } = true;
+        public bool ShowNotification { get; set; } = true;
+    }
+}
